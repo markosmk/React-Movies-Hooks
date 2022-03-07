29cf76d6883e469e9d862450971e20f27a107280
Markos <markosmk@me.com>
2022-03-06 23:04:30 -0300

    update: added new pages

---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+export { default as Home } from './Home/Home';
+export { default as MovieDetail } from './Movies/MovieDetail';
+export { default as Search } from './Search/Search';
+export { default as Series } from './Series/Series';
+export { default as Movies } from './Movies/Movies';
+export { default as Upcoming } from './Upcoming/Upcoming';
