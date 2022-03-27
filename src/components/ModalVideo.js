1bcb93fdcd6c36f226b51ed0875a8ce67cef608f
Markos <markosmk@me.com>
2022-03-26 23:12:58 -0300

    update: minos changes

--- src/components/ModalVideo.js
+++ src/components/ModalVideo.js
@@ -13,25 +13,25 @@ function ModalVideo({ isOpen, onClose, videoId }) {
 
   return (
     <div
-      class="fixed z-50 inset-0 overflow-y-auto"
+      className="fixed z-50 inset-0 overflow-y-auto"
       aria-labelledby="modal-youtube"
       role="dialog"
       aria-modal="true"
     >
-      <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+      <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
         <div
-          class="fixed inset-0 bg-black bg-opacity-75 transition-opacity"
+          className="fixed inset-0 bg-black bg-opacity-75 transition-opacity"
           aria-hidden="true"
           onClick={onClose}
         ></div>
 
         <span
-          class="hidden sm:inline-block sm:align-middle sm:h-screen"
+          className="hidden sm:inline-block sm:align-middle sm:h-screen"
           aria-hidden="true"
         >
           &#8203;
         </span>
-        <div class="relative inline-block align-bottom bg-black rounded-lg overflow-hidden  shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-5xl sm:w-full">
+        <div className="relative inline-block align-bottom bg-black rounded-lg overflow-hidden  shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-5xl sm:w-full">
           {videoLoading && (
             <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white">
               Loading..
