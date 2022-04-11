8135442b81618476502d91ec69e9dfa7b62bef13
Markos <markosmk@me.com>
2022-04-11 20:02:14 -0300

    add: subheader and formsearch component

---
+++ src/components/HeaderPage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function HeaderPage({ title, description, children }) {
+  return (
+    <div className="p-12 bg-slate-100 dark:bg-slate-700 text-center">
+      <h2 className="mb-3 text-5xl font-bold text-black dark:text-white uppercase">
+        {title}
+      </h2>
+      <p className="mb-0 py-4 text-slate-600 dark:text-slate-400">{description}</p>
+      {children && (
+        <div className="container mt-4">
+          <div className="max-w-lg mx-auto">{children}</div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default HeaderPage;
