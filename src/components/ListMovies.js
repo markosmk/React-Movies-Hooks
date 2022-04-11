077fd365564eb4800dc799b78a04c2422d8d5cd6
Markos <markosmk@me.com>
2022-04-11 20:02:39 -0300

    update: listmovies with spinner loaded

--- src/components/ListMovies.js
+++ src/components/ListMovies.js
@@ -1,15 +1,40 @@
 import CardMovie from 'components/CardMovie';
 import Pagination from 'components/Pagination';
+import { SpinnerCard } from 'components/Spinner';
 
-function ListMovies({ listing, currentPage, setPage, totalPages }) {
+function ListMovies({
+  isLoading,
+  spinners = 8,
+  listing,
+  currentPage,
+  setPage,
+  totalPages,
+  type,
+}) {
+  if (isLoading) {
+    return <SpinnerCard count={spinners} />;
+  }
+
   return (
     <>
-      <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 my-4">
-        {listing.length > 0 &&
-          listing.map((item) => <CardMovie key={item.id} {...item} />)}
-      </div>
-      {setPage && totalPages && (
-        <Pagination currentPage={currentPage} setPage={setPage} totalPages={totalPages} />
+      {listing.length === 0 ? (
+        <p className="flex justify-center my-6 text-slate-400">No results found</p>
+      ) : (
+        <>
+          <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 my-4">
+            {listing.map((item) => (
+              <CardMovie key={item.id} {...item} />
+            ))}
+          </div>
+          {setPage && totalPages && (
+            <Pagination
+              currentPage={currentPage}
+              setPage={setPage}
+              type={type}
+              totalPages={totalPages}
+            />
+          )}
+        </>
       )}
     </>
   );
