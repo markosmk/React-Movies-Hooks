c1a36a6def913308707ac6c60d3d0b58488ede67
Markos <markosmk@me.com>
2022-04-22 21:28:48 -0300

    add: page upcoming and series (tv) y detail

--- src/pages/Series/Series.js
+++ src/pages/Series/Series.js
@@ -1,13 +1,16 @@
 import { useEffect, useState } from 'react';
 
 import ListMovies from 'components/ListMovies';
+import { SpinnerCard } from 'components/Spinner';
 import useStore from 'store';
+import HeaderPage from 'components/HeaderPage';
+import shallow from 'zustand/shallow';
 
 function Series() {
-  // const isLoading = useStore((state) => state.tv?.isLoading);
-  const results = useStore((state) => state.tv.results);
-  const total_pages = useStore((state) => state.tv?.total_pages);
-  const getTvSeries = useStore((state) => state.getTvSeries);
+  const isLoading = useStore((state) => state.tv?.isLoading);
+  const results = useStore((state) => state.tv.results, shallow);
+  const total_pages = useStore((state) => state.tv?.total_pages, shallow);
+  const getTvSeries = useStore((state) => state.getTvSeries, shallow);
 
   const [page, setPage] = useState(1);
 
@@ -16,22 +19,25 @@ function Series() {
   }, [page, getTvSeries]);
 
   return (
-    <div className="container">
-      <div className="p-24 bg-gray-100 rounded-xl text-center">
-        <h2 className="mb-3 text-5xl font-bold text-black">Series</h2>
-        <p className="mb-0 text-sm leading-7 py-4">
-          This year is presented as an epic year in the world of series, all platforms
-          revolutionize their catalogs
-        </p>
-      </div>
-
-      <ListMovies
-        listing={results}
-        currentPage={page}
-        setPage={setPage}
-        totalPages={total_pages}
+    <>
+      <HeaderPage
+        title="Series"
+        description="This year is presented as an epic year in the world of series, all platforms
+        revolutionize their catalogs"
       />
-    </div>
+      <div className="container">
+        {isLoading ? (
+          <SpinnerCard />
+        ) : (
+          <ListMovies
+            listing={results}
+            currentPage={page}
+            setPage={setPage}
+            totalPages={total_pages}
+          />
+        )}
+      </div>
+    </>
   );
 }
 
