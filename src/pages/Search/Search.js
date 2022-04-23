396f5c1ffd9d45f1d8a435f850108e76e1efd8a6
Markos <markosmk@me.com>
2022-04-22 21:16:12 -0300

    add: page genre and filter, update search page

--- src/pages/Search/Search.js
+++ src/pages/Search/Search.js
@@ -1,77 +1,114 @@
-import { useState, useRef, useEffect } from 'react';
+import { useEffect } from 'react';
 import { useSearchParams } from 'react-router-dom';
 
-import { SearchIcon } from '@heroicons/react/solid';
+import { Tab } from '@headlessui/react';
+import ListPersons from 'components/ListPersons';
 import ListMovies from 'components/ListMovies';
+import HeaderPage from 'components/HeaderPage';
+import FormSearch from 'components/FormSearch';
+import { SpinnerCardWithFilter } from 'components/Spinner';
+
 import useStore from 'store/index';
+import shallow from 'zustand/shallow';
+import { classNames } from 'helpers';
 
 function Search() {
-  const { results, total_pages, total_results } = useStore((state) => state.search);
-  const getSearchMovies = useStore((state) => state.getSearchMovies);
+  const { total, movie, tv, person } = useStore((state) => state.search, shallow);
+  const isLoading = useStore((state) => state.search.isLoading);
+  const getSearchMovies = useStore((state) => state.getSearchMovies, shallow);
+  const getPageSearch = useStore((state) => state.getPageSearch, shallow);
 
-  const inputSearch = useRef(null);
-  const [searchHeader, setSearchHeader] = useSearchParams({});
-  const [search, setSearch] = useState('');
-
-  console.log('searchHeader Params', searchHeader.get('q'));
-  console.log('search', search);
-
-  // if submit form change list movies
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    getSearchMovies(search);
-    setSearchHeader({ q: search });
-  };
+  const [searchHeader] = useSearchParams({});
 
   // if change page update list movies
-  const setPage = (newPage) => {
-    getSearchMovies(search, newPage);
+  const setPage = (newPage, type) => {
+    getPageSearch(searchHeader.get('q'), newPage, type);
   };
 
   // if exists query 'q' in urlParams update list movies
   useEffect(() => {
     if (searchHeader.get('q')) {
-      inputSearch.current.value = searchHeader.get('q');
-      setSearch(searchHeader.get('q'));
       getSearchMovies(searchHeader.get('q'));
     }
   }, [searchHeader, getSearchMovies]);
 
+  const tabSections = [
+    { title: 'Movies', count: movie?.total_results || 0 },
+    { title: 'Tv Shows', count: tv?.total_results || 0 },
+    { title: 'Persons', count: person?.total_results || 0 },
+  ];
+
   return (
-    <div className="container">
-      <div className="p-24 bg-gray-100 rounded-xl text-center">
-        <h2 className="mb-3 text-5xl font-bold text-black">
-          {search
-            ? total_results + ' found for ' + search || searchHeader.get('q') + '.'
-            : 'Search Movies!'}
-        </h2>
-        <p className="mb-0 text-sm leading-7 py-4">
-          Get recommendations for new movies to watch, rent, stream, or own.
-        </p>
+    <>
+      <HeaderPage
+        title={
+          searchHeader.get('q')
+            ? total + ' found for ' + searchHeader.get('q')
+            : 'Search Movies!'
+        }
+        description="Get recommendations for new movies to watch, rent, stream, or own."
+      >
+        <FormSearch />
+      </HeaderPage>
 
-        <form onSubmit={handleSubmit}>
-          <label className="relative block mr-4">
-            <span className="sr-only">Search</span>
-            <span className="absolute inset-y-0 left-0 flex items-center pl-2">
-              <SearchIcon className="h-5 w-5 fill-slate-300" />
-            </span>
-            <input
-              className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
-              placeholder="Search for movie..."
-              type="text"
-              name="search"
-              ref={inputSearch}
-              onChange={(e) => {
-                const text = inputSearch.current.value;
-                setSearch(text);
-              }}
-            />
-          </label>
-        </form>
-      </div>
-
-      <ListMovies listing={results} setPage={setPage} totalPages={total_pages} />
-    </div>
+      {isLoading ? (
+        <div className="container pt-6">
+          <SpinnerCardWithFilter />
+        </div>
+      ) : (
+        <div className="container pt-6">
+          <Tab.Group>
+            <Tab.List className="flex p-2 space-x-1 bg-slate-50 dark:bg-slate-800 rounded-lg">
+              {tabSections.map((tab) => (
+                <Tab
+                  key={tab.title}
+                  className={({ selected }) =>
+                    classNames(
+                      'w-full py-3 text-sm leading-5 font-medium text-slate-400 rounded-md transition-colors',
+                      selected
+                        ? 'bg-cyan-500 text-cyan-100'
+                        : 'hover:bg-slate-200 dark:hover:bg-slate-600 hover:text-cyan-900 dark:hover:text-slate-200'
+                    )
+                  }
+                >
+                  {tab.title}
+                  <span className="ml-2 font-bold">{tab.count}</span>
+                </Tab>
+              ))}
+            </Tab.List>
+            <Tab.Panels>
+              <Tab.Panel>
+                <ListMovies
+                  listing={movie?.results}
+                  currentPage={movie?.page}
+                  setPage={setPage}
+                  type="movie"
+                  totalPages={movie?.total_pages}
+                />
+              </Tab.Panel>
+              <Tab.Panel>
+                <ListMovies
+                  listing={tv?.results}
+                  currentPage={tv?.page}
+                  setPage={setPage}
+                  type="tv"
+                  totalPages={tv?.total_pages}
+                />
+              </Tab.Panel>
+              <Tab.Panel>
+                <ListPersons
+                  listing={person?.results}
+                  currentPage={person?.page}
+                  setPage={setPage}
+                  type="person"
+                  totalPages={person?.total_pages}
+                />
+              </Tab.Panel>
+            </Tab.Panels>
+          </Tab.Group>
+        </div>
+      )}
+    </>
   );
 }
 
