45cbbab01a4f82c6574f67004026257c963820e6
markosmk <markosmk@me.com>
2023-10-28 22:54:31 -0300

    update: conver to typescript component

--- src/components/Score.tsx
+++ src/components/Score.tsx
@@ -1,27 +1,27 @@
-function Score({ vote, text, url }) {
+interface ScoreProps {
+  vote: {
+    class: string;
+    count: string;
+  };
+  text?: string;
+  url?: string;
+}
+
+export const Score = ({ vote, text, url }: ScoreProps) => {
   return (
     <div className={`relative inline-flex items-center ${vote.class} text-sm`}>
       <svg
-        className={`w-10 align-middle overflow-hidden -rotate-90 origin-center transform ${
-          text && `mr-2`
-        }`}
+        className={`w-10 align-middle overflow-hidden -rotate-90 origin-center transform ${text && `mr-2`}`}
         viewBox="0 0 30 30"
         width="40"
         height="40"
         xmlns="http://www.w3.org/2000/svg"
       >
-        <circle
-          className="stroke-slate-700"
-          strokeWidth="2"
-          fill="none"
-          cx="15"
-          cy="15"
-          r="14"
-        ></circle>
+        <circle className="stroke-slate-700" strokeWidth="2" fill="none" cx="15" cy="15" r="14"></circle>
         <circle
           className={`${vote.class}`}
           strokeWidth="2"
-          strokeDasharray={`${vote.count * 9},100`}
+          strokeDasharray={`${Number(vote.count) * 9},100`}
           cx="15"
           cy="15"
           r="14"
@@ -31,14 +31,11 @@ function Score({ vote, text, url }) {
         {vote.count === '0.0' ? 'NR' : vote.count}
       </b>
       {text && url && (
-        <a href={url} target="_blank" className={vote.class} rel="noreferrer" alt="IMDb">
-          {' '}
-          {text}
+        <a href={url} target="_blank" className={vote.class} rel="noreferrer" aria-label="IMDb">
+          {` ${text}`}
         </a>
       )}
       {text && !url && ` ${text}`}
     </div>
   );
-}
-
-export default Score;
+};
